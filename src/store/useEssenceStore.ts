2888ff00f54cb2207941f5557271c46717f6d286
Paulo Henrique Calletti <pauloonlline@gmail.com>
2025-06-06 16:17:44 -0300

    Mais ajustes no TurnController

--- src/store/useEssenceStore.ts
+++ src/store/useEssenceStore.ts
@@ -2,16 +2,19 @@ import { create } from "zustand";
 
 interface EssenceState {
   essence: number;
+  maxEssence: number;
   addEssence: () => void;
   spendEssence: (amount: number) => boolean;
   resetEssence: () => void;
+  increaseMaxEssence: () => void;
 }
 
 export const useEssenceStore = create<EssenceState>((set, get) => ({
   essence: 1, // Start with 1 essence
+  maxEssence: 1, // Start with 1 max essence
   
   addEssence: () => set((state) => ({ 
-    essence: state.essence + 1 
+    essence: Math.min(state.essence + 1, state.maxEssence) // Can't exceed max essence
   })),
   
   spendEssence: (amount: number) => {
@@ -23,5 +26,13 @@ export const useEssenceStore = create<EssenceState>((set, get) => ({
     return false;
   },
   
-  resetEssence: () => set({ essence: 1 }),
+  resetEssence: () => set({ 
+    essence: 1,
+    maxEssence: 1
+  }),
+
+  increaseMaxEssence: () => set((state) => ({
+    maxEssence: state.maxEssence + 1,
+    essence: state.maxEssence + 1 // Fill up to new max essence
+  })),
 })); 
